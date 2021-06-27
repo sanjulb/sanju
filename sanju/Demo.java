83121a826e04375baef586aaafa9fdda0d36b45d
sanjulb <86560800+sanjulb@users.noreply.github.com>
2021-06-27 19:53:58 +0530

    Add files via upload
    java notes

---
+++ sanju/Demo.java
@@ -0,0 +1,16 @@
+
+class Demo {
+
+  public static void main(String args[]) {
+
+       String one = args[0];
+String two = args[1];
+System.out.println(args[0]);
+//
+System.out.println(args[1]);
+
+
+
+
+
+
